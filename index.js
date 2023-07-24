e20ad565067ec695ca7e81995f9df223fd84929a
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-07-24 16:08:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-document.write('<script src="js/index.js"></script>');
+document.addEventListener('DOMContentLoaded', function () {
+    const unionBar = document.querySelector('.union-bar');
+    const crossBtn = document.querySelector('.cross-btn');
+    const popUpSection = document.querySelector('.pop-up-section');
+    const popUpMenu = document.querySelector('.pop-up');
+
+    unionBar.addEventListener('click', popUp());
+    crossBtn.addEventListener('click', popOut());
+
+    function popUp() {
+        popUpSection.style.display = 'block';
+    }
+
+    function popOut() {
+        popUpSection.style.display ='none';
+    }
+});
