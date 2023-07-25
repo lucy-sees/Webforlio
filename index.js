c46aa64959349d302964085598543f051af89b7b
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-07-25 13:56:11 +0300

    Add javascript functionality to the pop-up menu

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const unionBar = document.querySelector('.union-bar');
-    const crossBtn = document.querySelector('.cross-btn');
-    const popUpSection = document.querySelector('.pop-up-section');
-    const popUpMenu = document.querySelector('.pop-up');
+// Get the elements for the pop-up menu and the union-bar
+const popUpSection = document.querySelector('.pop-up-section');
+const unionBar = document.querySelector('.union-bar');
+const crossBtn = document.querySelector('.cross-btn');
+const popUpItems = document.querySelectorAll('.pop-menu-item');
 
-    unionBar.addEventListener('click', popUp());
-    crossBtn.addEventListener('click', popOut());
+// Add a click event listener to the union-bar
+unionBar.addEventListener('click', togglePopUp);
 
-    function popUp() {
-        popUpSection.style.display = 'block';
-    }
+// Function to toggle the visibility of the pop-up menu
+function togglePopUp() {
+  popUpSection.classList.toggle('show');
+}
 
-    function popOut() {
-        popUpSection.style.display ='none';
-    }
+// Add a click event listener to each pop-up menu item
+popUpItems.forEach(item => {
+  item.addEventListener('click', hidePopUp);
 });
+
+// Function to hide the pop-up menu when a menu item is clicked
+function hidePopUp() {
+  popUpSection.classList.remove('show');
+}
+
+// Add a click event listener to the cross button
+crossBtn.addEventListener('click', hidePopUpWithCross);
+
+// Function to hide the pop-up menu when the cross button is clicked
+function hidePopUpWithCross() {
+  popUpSection.classList.remove('show');
+}
+
+ // Scroll smoothly to the section when a menu item is clicked
+ function smoothScrollToSection(e) {
+  e.preventDefault();
+  const target = document.querySelector(e.target.getAttribute('href'));
+  if (target) {
+    const yOffset = -50; // Optional offset if you have a sticky header
+    const y = target.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({ top: y, behavior: 'smooth' });
+  }
+  hidePopUp();
+}
+
+popUpItems.forEach(item => item.addEventListener('click', smoothScrollToSection));
